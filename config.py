3ae571704ce9dd3602e2a25eca448a08e767c6e5
Lemax7 <olexxx2006@gmail.com>
2022-06-17 15:00:00 +0300

    added README + fixed config.py + updated requirements.txt

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import configparser
 from sqlalchemy import create_engine
 
 db_config = configparser.ConfigParser()
-db_config.read('/Users/max/Yandex.Disk.localized/Documents/Scripts/dashboards/config.txt')
+db_config.read('config.txt')
 
 engine = create_engine(db_config.get('database', 'con'))
